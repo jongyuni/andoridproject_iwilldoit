cf9321a25b32c65514e6d33316a5aac6644f6a05
Choi Jongyun <popi0905@gmail.com>
2021-11-25 21:48:42 +0900

    version 1.1

--- app/src/main/java/com/example/iwilldoit/MainActivity.java
+++ app/src/main/java/com/example/iwilldoit/MainActivity.java
@@ -1,32 +1,54 @@
 package com.example.iwilldoit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.iwilldoit.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        firebaseAuth = FirebaseAuth.getInstance();
         View view = binding.getRoot();
         setContentView(view);
     }
 
     public void loginClick(View view){
-        Intent intent = new Intent(MainActivity.this,HomeActivity.class);
-        startActivity(intent);
+        String email = binding.editId.getText().toString().trim();
+        String pwd = binding.editPwd.getText().toString().trim();
+        firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(MainActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void signupClick(View view){
         Intent intent = new Intent(MainActivity.this,SignupActivity.class);
         startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/iwilldoit/SignupActivity.java
+++ app/src/main/java/com/example/iwilldoit/SignupActivity.java
@@ -1,21 +1,67 @@
 package com.example.iwilldoit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.iwilldoit.databinding.ActivityMainBinding;
+import com.example.iwilldoit.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private ActivitySignupBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
+        firebaseAuth = FirebaseAuth.getInstance();
         setContentView(view);
+    }
+
+    public void confirmClick(View view){
+
+        String id = binding.userId.getText().toString().trim();
+        String password = binding.userPassword.getText().toString().trim();
+        String passwordCheck = binding.userPasswordCheck.getText().toString().trim();
+
+        if(id.length()>0 && password.length()>0 && passwordCheck.length()>0){
+            if(password.equals(passwordCheck)){
+                firebaseAuth.createUserWithEmailAndPassword(id, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignupActivity.this, "회원가입에 성공했습니다." ,Toast.LENGTH_SHORT).show();
+                                } else {
+                                    if(task.getException().toString() !=null){
+                                        Toast.makeText(SignupActivity.this, "회원가입에 실패했습니다." ,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        });
+            }
+            else{
+                Toast.makeText(SignupActivity.this, "비밀번호가 일치하지 않습니다." ,Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(SignupActivity.this, "아아디와 비밀번호를 확인해주세요." ,Toast.LENGTH_SHORT).show();
+        }
+
+        Intent intent = new Intent(SignupActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
